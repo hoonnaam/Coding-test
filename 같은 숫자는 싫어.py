842eb8e093ae3be0b24ee792c94c95992e09e9d0
hoonnaam <ckswl320@naver.com>
2020-09-23 23:57:38 +0900

    modified hate same number

--- 같은 숫자는 싫어.py
+++ 같은 숫자는 싫어.py
@@ -2,12 +2,15 @@ def solution(arr):
     answer = []
     for idx, number in enumerate(arr):
         # 첫번째 숫자 인덱싱
-        if idx == 0
+        if idx == 0:
             answer.append(number)
 
         # 연속적 수가 일 경우 인덱싱
         else:
-            if
+            if arr[idx] == arr[idx+1]:
+                answer.remove(number)
+
+        print(answer)
 
         # 연속적 수가 아닐 경우 제거print('Hello Python')
-        return 0
+        return answer
