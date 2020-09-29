70d62ca5d930fa4d3c178ae62009044257edfd25
hoonnaam <ckswl320@naver.com>
2020-09-29 17:49:18 +0900

    finished code

--- 문자열 다루기 기본.py
+++ 문자열 다루기 기본.py
@@ -1,6 +1,6 @@
 def solution(s):
     # 길이가 4 or 6 인지 확인
-    if len(s) == 4 & len(s) == 6:
+    if len(s) == 4 or len(s) == 6:
         # 숫자로만 구성돼 있는지 확인
         if s.isdigit():
             answer = True
@@ -9,3 +9,7 @@ def solution(s):
     else:
         answer = False
     return answer
+
+s="a234"
+# s="1235"
+print(solution(s))
