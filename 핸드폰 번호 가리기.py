53d1cddeaefe84bce2a83fc559782176b02e12a0
hoonnaam <ckswls320@google.com>
2020-10-04 23:54:41 +0900

    modified code

--- 핸드폰 번호 가리기.py
+++ 핸드폰 번호 가리기.py
@@ -1,6 +1,8 @@
 def solution(phone_number):
-    answer = ''
     # 뒤에서 5자리 수부터 *  표시 [-5:]
     for i in phone_number[-5::-1]:
+        table = str.maketrans('123456789', '*')
+        phone_number.translate(table)
+        print(phone_number)
 
-    return answer
+    return phone_number
