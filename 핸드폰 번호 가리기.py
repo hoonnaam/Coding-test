6f82aefd95894e77d9a3a888a5a08dba0310bee0
hoonnaam <ckswl320@naver.com>
2020-10-06 13:52:17 +0900

    finished secure_number

--- 핸드폰 번호 가리기.py
+++ 핸드폰 번호 가리기.py
@@ -1,19 +1,12 @@
-# def solution(phone_number):
-#     # 뒤에서 5자리 수부터 *  표시 [-5:]
-#     for i in phone_number[-5::-1]:
-#         table = str.maketrans('123456789', '*')
-#         phone_number.translate(table)
-#         print(phone_number)
-#
-#     return phone_number
+def solution(phone_number):
+    # 뒤에서 5자리 수부터 *  표시 [-4:]
+    front = (len(phone_number) - 4) * '*'
+    end = phone_number[-4:]
 
-phone_number = "01073725467"
-# 연산
-# for i in phone_number[0:-4]:
-#     print(i)
-# table = str.maketrans(', '*')
-# secure_number = phone_number[:4].translate(str.maketrans('1234567890', '**********'))
-secure_number = (len(phone_number) - 4) * '*'
-vision_number = phone_number[6:-1]
-print(secure_number)
-print(vision_number)
+    return front + end
+
+phone_number = "01073725466"
+# phone_number = "027778888"
+
+print(solution(phone_number))
+
