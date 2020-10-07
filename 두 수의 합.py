57132831f29e188c2d1c33d11da0d7c7ed1e1167
hoonnaam <ckswl320@naver.com>
2020-10-07 23:44:38 +0900

    add alice code

---
+++ 두 수의 합.py
@@ -0,0 +1,19 @@
+
+# def twoSum(nums, target):
+# 	# 하나의 nums가 target 보다 크다면 pass
+# 	# 두 nums의 합이 target 가 일치하는지
+# 	# 일치한다면 두 nums 출력
+#     return 0,0
+#
+# def main():
+#     print(twoSum([2, 8, 19, 37, 4, 5], 12))
+#
+# if __name__ == "__main__":
+#     main()
+
+
+nums = [2,8,19,37,4,5]
+
+print(type(nums))
+nums.sort()
+print(nums)
