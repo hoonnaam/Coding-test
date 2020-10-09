88c1a328020c9c5246a1efdc279516e6a65ca7ed
hoonnaam <ckswls320@google.com>
2020-10-09 23:53:52 +0900

    modified code

--- 두 수의 합.py
+++ 두 수의 합.py
@@ -1,4 +1,4 @@
-def twoSum(nums, target):
+def twoSum_plus(nums, target):
     # 하나의 nums가 target 보다 크다면 pass
     nums.sort()
     for idx, i in enumerate(nums):
@@ -9,13 +9,13 @@ def twoSum(nums, target):
     for i in range(len(nums)):
         for p in (range(len(nums) - 1)):
             if nums[i] + nums[p + 1] == target:
-                # 일치한다면 두 nums 출력
 
+                # 일치한다면 두 nums 출력
                 return nums[i], nums[p + 1]
 
 
 def main():
-    print(twoSum([2, 8, 19, 37, 4, 5], 12))
+    print(twoSum_plus([2, 8, 19, 37, 4, 5, 9, 13], 12))
 
 
 if __name__ == "__main__":
