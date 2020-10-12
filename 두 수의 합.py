488f1edfba460a9b5126f747ac188dbbc0e49d63
hoonnaam <ckswl320@naver.com>
2020-10-12 23:44:39 +0900

    upgrade alice_code

--- 두 수의 합.py
+++ 두 수의 합.py
@@ -1,9 +1,9 @@
 def twoSum(nums, target):
     # 하나의 nums가 target 보다 크다면 pass
     nums.sort()
-    for idx, i in enumerate(nums):
-        if i > target:
-            nums = nums[:idx]
+    # for idx, i in enumerate(nums):
+    #     if i > target:
+    #         nums = nums[:idx]
 
     # 두 nums의 합이 target 가 일치하는지
     for i in range(len(nums)):
@@ -13,6 +13,18 @@ def twoSum(nums, target):
 
                 return nums[i], nums[p + 1]
 
+    # 효율성을 높이기 위한 함수 수정 반복문2회 -> 1회
+    i = 0
+    j = len(nums) - 1
+
+    while i < j:
+        sum = nums[i] + nums[j]
+        if sum == target:
+            return nums[i], nums[j]
+        elif sum < target:
+            i += 1
+        else:
+            j -= 1
 
 def main():
     print(twoSum([2, 8, 19, 37, 4, 5], 12))
