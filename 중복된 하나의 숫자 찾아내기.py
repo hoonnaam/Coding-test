dfe4c40b078a89d00106bad5d2a1c82392de9909
hoonnaam <ckswl320@naver.com>
2020-10-13 23:38:38 +0900

    add alice_code

---
+++ 중복된 하나의 숫자 찾아내기.py
@@ -0,0 +1,17 @@
+def findDuplicate(nums):
+    nums.sort()
+
+    i = 0
+    while i < len(nums) - 1:
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+        else:
+            i += 1
+
+
+def main():
+    print(findDuplicate([1, 5, 2, 4, 5, 6, 3]))
+
+
+if __name__ == "__main__":
+    main()
