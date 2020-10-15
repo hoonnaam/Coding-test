b4e0a9930bb229c238770fb33f462232821e3b2e
hoonnaam <ckswl320@naver.com>
2020-10-15 21:36:34 +0900

    finished alice_code

--- 가장 큰 부분합 구하기.py
+++ 가장 큰 부분합 구하기.py
@@ -1,11 +1,12 @@
 def maxSubArray(nums):
+
+    result = []
     # 반복문을 통해 슬라이싱
+    for n in range(len(nums)):
+        for p in range(len(nums)):
+            result.append(sum(nums[n:p + 1]))
 
-    # 해당 리스트의 총합을 result_1 에 대입
-
-    # 만약 result_2 가 reslut_1에 대입
-
-    return result
+    return max(result)
 
 
 def main():
