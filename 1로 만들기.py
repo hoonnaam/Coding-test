76f390dbbd5539a412d28425ce52b346a7293382
hoonnaam <ckswl320@naver.com>
2020-10-19 23:50:48 +0900

    modifed alice_code

--- 1로 만들기.py
+++ 1로 만들기.py
@@ -1,7 +1,18 @@
 def convertTo1(num):
-    # 2와 3의 배수가 아닐 시 빼기 1
 
-    return 0
+    # 2의 배수 num % 2 == 0
+    if num % 2 == 0:
+        num /= 3
+
+    # 3의 배수 num % 3 == 0
+    elif num % 3 == 0:
+        num /= 3
+
+    # 2와 3의 배수가 아닐 시 빼기 1
+    else:
+        num -= 1
+
+    return num
 
 
 def main():
