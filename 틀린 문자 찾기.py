e9131ed02b17226479518809c618a8782b488469
hoonnaam <ckswl320@naver.com>
2020-10-28 00:31:52 +0900

    finished alice_code

--- 틀린 문자 찾기.py
+++ 틀린 문자 찾기.py
@@ -3,11 +3,11 @@ def findDifference(str1, str2):
     str2List = list(str2)
     str1List.sort()
     str2List.sort()
+    str1List.append(0)
 
     for n in range(len(str1List)):
         if str1List[n] != str2List[n]:
-            return
-
+            return str2List[n]
 
 def main():
     print(findDifference("apple", "azlppe"))
